8b025cd93fd6ae605b83c0845c7e9541f3e917ba
admin <admin>
2016-08-12 21:04:21 +0000

    Export: 2nd commit

--- Tutorial_HL7_to_Database-To-tqBSozBVQSaU5v/main.lua
+++ Tutorial_HL7_to_Database-To-tqBSozBVQSaU5v/main.lua
@@ -4,71 +4,30 @@
 -- MySQL, Microsoft SQL Server, Oracle, Maria DB etc. etc.
 -- PLEASE READ THE TODO statement on line 38 and follow it.
 
-require 'create_database'
-
--- This function is called by the main function lower down
-function MapPatient(Patient, PID)
-   Patient.Id        = PID[3][1][1]
-   Patient.FirstName = PID[5][1][2]
-   Patient.LastName  = PID[5][1][1][1]
-   Patient.Gender    = PID[8]   
-   -- Click the Patient Row to the right to see results
-   return Patient
-end
-
--- This shows how we can model a 'one to many'data relationship of a Patient to Kin.
--- This function is called in a loop from the main function
-
-function MapKin(Kin, NK1, PatientId)
-   Kin.PatientId    = PatientId
-   Kin.FirstName    = NK1[2][1][2]
-   Kin.LastName     = NK1[2][1][1][1]
-   Kin.Relationship = NK1[3][1]
-   -- Click the Kin Row to the right to see results
-   return Kin
-end
 
 -- This is the main entry point in the translator, Data contains the HL7 message from sample data
 function main(Data)
    -- By default the SQLite database is stored in iguana.workingDir()
-   DB = db.connect{api=db.SQLITE, name='DemoDatabase.sqlite'}
-   local Info = DatabaseLocation(DB)
-	trace(Info)
-   -- The Patient and Kin tables will be automatically created 
-   -- TODO - Please comment out CreateTables(DB)
-   -- after you have run the code the first time.
-   CreateTables(DB)
+   DB = db.connect{api=db.ORACLE_OCI, name= 'localdb' , user='TDRUN', password='Smrt600' }
+
    -- Parse incoming raw message with hl7.parse
    local MsgIn = hl7.parse{vmd='demo.vmd', data=Data}
-   -- Build a table node tree structure to temporarily store 
-   -- Patient and Kin data before saving it to the database 
-   local TableOut = db.tables{vmd='demo_tables.vmd', name='ADT'}
+
+   -- clone message into out variable
+   local MsgOut = hl7.message{vmd='demo.vmd', name='ADT'}
+   MsgOut:mapTree(MsgIn)   
    
-   -- Map fields from PID segment to Patient table
-   MapPatient(TableOut.Patient[1], MsgIn.PID)
+   -- transform message here
    
-   -- Map Kin for patient in one or more NK1 segments
-   -- See how we loop through repeating data with a for loop?
-   trace("We have "..#MsgIn.NK1.." kin.")
-   for i=1, #MsgIn.NK1 do
-      MapKin(TableOut.Kin[i],MsgIn.NK1[i],MsgIn.PID[3][1][1])
-   end
-   -- Insert information from TableOut into the database tables
-   -- NOTE: To stop inserts being made live in the Editor set
-   -- live=false
-   DB:merge{data=TableOut, live=true}
+   -- write to database using stored procedure
+   local PatId = MsgOut.PID[3][1][1]:S()
+   local VisitNum = MsgOut.PID[18][1]:S()
+   local MsgOutStr = MsgOut:S()
+   local SQL = "CALL td_iguana_pkg.insert_iguana_queue('ADT',".. DB:quote(MsgOutStr)..","..DB:quote(PatId)..","..DB:quote(VisitNum)..")"
+   trace(SQL)
+   DB:execute(SQL)
    
-   -- View data by clicking on Result Sets in annotations 
-   DB:query{sql='Select * from Patient'}
-   DB:query{sql='Select * from Kin'}
-   
-   -- Set live=true to clear data for the Patient and/or Kin table
-   DB:execute{sql='Delete from Patient', live=false}   
-   DB:execute{sql='Delete from Kin', live=false} 
    DB:close()
    
 end
 
-
--- Suggest that you click around on the Purple function calls in the annotation blocks on the right to
--- navigate around the code and open up dialogs to see the data in it's various stages.
